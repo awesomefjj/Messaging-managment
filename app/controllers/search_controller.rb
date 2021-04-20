33f78b4f6f643917318199105cfef2c9f4755503
Jeffery <522023893@qq.com>
2021-04-20 10:33:02 +0800

    页面优化

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,13 @@
 class SearchController < ApplicationController
 before_action :s_tenant, only: %i[show]
     def index
-       
     end
 
     def show
-        @searching = Monitoring.all
-        
+      @searching = Monitoring.all
     end
     
     def new
-        
     end
 
 
@@ -20,7 +17,7 @@ private
 def s_tenant
     
     
- @searching = Monitoring.find_by(receive_type: params[:receive_type])
+ @searching = Monitoring.find_by(tenant_id: params[:tenant_id])
       
     
 
